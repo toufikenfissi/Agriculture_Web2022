0c087cc280690ddf9316ca2611cce78b0554dbb7
toufikenfissi <97600227+toufikenfissi@users.noreply.github.com>
2022-03-01 01:04:24 +0100

    Add files via upload

---
+++ principaux_villages.js
@@ -0,0 +1,14 @@
+var principaux_villages={
+"type": "FeatureCollection",
+"name": "villages",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "osm_id": "1967584144", "name": "Oulad Bourima أولاد بوريمة", "type": "village", "population": null }, "geometry": { "type": "Point", "coordinates": [ -3.7492089, 34.3538462 ] } },
+{ "type": "Feature", "properties": { "osm_id": "1977166094", "name": "Oulad Fennane أولاد فنان", "type": "village", "population": null }, "geometry": { "type": "Point", "coordinates": [ -6.5770559, 32.9858073 ] } },
+{ "type": "Feature", "properties": { "osm_id": "2005878533", "name": "Bab Tiouka باب تيوكا", "type": "village", "population": null }, "geometry": { "type": "Point", "coordinates": [ -5.6625941, 34.245491 ] } },
+{ "type": "Feature", "properties": { "osm_id": "2030386942", "name": "Lakbayl", "type": "village", "population": null }, "geometry": { "type": "Point", "coordinates": [ -9.6668064, 31.0823421 ] } },
+{ "type": "Feature", "properties": { "osm_id": "2882514307", "name": "Kef El Mouneb", "type": "village", "population": null }, "geometry": { "type": "Point", "coordinates": [ -7.9452368, 32.4335786 ] } },
+{ "type": "Feature", "properties": { "osm_id": "2911631628", "name": null, "type": "village", "population": null }, "geometry": { "type": "Point", "coordinates": [ -4.9643751, 32.7889113 ] } },
+{ "type": "Feature", "properties": { "osm_id": "2953206355", "name": null, "type": "village", "population": null }, "geometry": { "type": "Point", "coordinates": [ -5.9349139, 32.0255849 ] } }
+]
+}
